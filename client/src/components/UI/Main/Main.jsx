31c4821269974e56e619a973ab3105a20935ff45
Cyril Marois <cyrilmarois@gmail.com>
2022-11-30 23:49:24 +0100

    feat(Notice): Add notices

--- client/src/components/UI/Main/Main.jsx
+++ client/src/components/UI/Main/Main.jsx
@@ -1,11 +1,15 @@
 import "./Main.css";
 
 import Filters from "./Filters/Filters";
+import NoticeWrongNetwork from "../Notice/NoticeWrongNetwork";
+import NoticeNoArtifact from "../Notice/NoticeNoArtifact";
 
 const Main = () => {
   return (
     <main>
       <Filters />
+      {/* <NoticeWrongNetwork />
+      <NoticeNoArtifact /> */}
     </main>
   );
 };

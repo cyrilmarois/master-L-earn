cc33f66410f07facceb29e41cf5e8a53aa0b28fb
Cyril Marois <cyrilmarois@gmail.com>
2022-12-04 00:43:50 +0100

    update login page

--- client/src/components/Pages/Login/Login.jsx
+++ client/src/components/Pages/Login/Login.jsx
@@ -4,28 +4,32 @@ const Login = () => {
   return (
     <section id="login" className="container">
       <div className="d-flex justify-content-evenly align-items-end  text-center">
-        <div className="card">
-          <h3 className="title">APPRENANT</h3>
+        <div className="card justify-content-center align-items-center pt-3">
+          <div className="title mb-3">
+            <i className="fa-solid fa-graduation-cap"></i>
+          </div>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
             aliquam erat, a vulputate justo. Suspendisse placerat scelerisque mi
             ut commodo.
           </p>
           <div className="align-items-end">
-            <a href="#" class="btn btn-primary">
-              Poursuivre
+            <a href="#" className="btn btn-primary">
+              DEVENIR APPRENANT
             </a>
           </div>
         </div>
-        <div className="card">
-          <h3 className="title">FORMATEUR</h3>
+        <div className="card justify-content-center align-items-center pt-3">
+          <div className="title mb-3">
+            <i className="fa-solid fa-person-chalkboard"></i>
+          </div>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id
             aliquam erat, a vulputate justo. Suspendisse placerat scelerisque mi
             ut commodo.
           </p>
-          <a href="#" class="btn btn-primary">
-            Poursuivre
+          <a href="#" className="btn btn-primary">
+            DEVENIR FORMATEUR
           </a>
         </div>
       </div>

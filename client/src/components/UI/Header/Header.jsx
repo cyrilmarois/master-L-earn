38d2779aa1231f10e90e3345dcbc3a0b0a13fb2c
Cyril Marois <cyrilmarois@gmail.com>
2022-12-04 00:39:03 +0100

    Header modal

--- client/src/components/UI/Header/Header.jsx
+++ client/src/components/UI/Header/Header.jsx
@@ -1,8 +1,51 @@
 import logo from "../../../images/mle-logo-sm.png";
 import SubHeader from "./SubHeader/SubHeader";
+import WalletButton from "../WalletButton/WalletButton";
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import Modal from "react-modal";
+
 import "./Header.css";
+import { useState } from "react";
+import ConnectModal from "./ConnectModal/ConnectModal";
+import useEth from "../../../contexts/EthContext/useEth";
 
 const Header = () => {
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [loggedAddress, setLoggedAddres] = useState("Connexion");
+  const {
+    state: { accounts, contract },
+  } = useEth();
+
+  console.log({ modalIsOpen });
+  let wallets = { metamask: false };
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  useEffect(() => {
+    const checkWallets = () => {
+      const tmpWallets = JSON.parse(localStorage.getItem("wallets"));
+      if (tmpWallets === null) {
+        let wallets = { metamask: false };
+        if (typeof window.ethereum !== "undefined") {
+          console.log("MetaMask is installed!");
+          wallets.metamask = true;
+        }
+        console.log({ wallets });
+        localStorage.setItem("wallets", JSON.stringify(wallets));
+      }
+    };
+    checkWallets();
+  }, []);
+
+  useEffect(() => {
+    if (contract && accounts && accounts[0]) {
+      setLoggedAddres(accounts[0].slice(0, 5) + "..." + accounts[0].slice(-4));
+    }
+  }, [accounts, contract]);
+
   return (
     <header>
       <div className="container">
@@ -26,9 +69,15 @@ const Header = () => {
                   </div>
 
                   <div className="col-1 offset-4">
-                    <button type="button" className="btn btn-primary">
-                      Connexion
+                    <button
+                      id="connect"
+                      onClick={openModal}
+                      type="button"
+                      className="btn btn-primary"
+                    >
+                      {loggedAddress}
                     </button>
+                    <ConnectModal modalIsOpen={modalIsOpen} />
                   </div>
                 </form>
               </div>

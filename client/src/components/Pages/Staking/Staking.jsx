c9098eeae08debd72ee935a50f7256f94cf0d99f
Cyril Marois <cyrilmarois@gmail.com>
2022-12-07 23:59:16 +0100

    feat(staking): staking WIP

--- client/src/components/Pages/Staking/Staking.jsx
+++ client/src/components/Pages/Staking/Staking.jsx
@@ -1,102 +1,120 @@
 import { useEffect, useState } from "react";
 import useEth from "../../../contexts/EthContext/useEth";
+import Plan from "./Plan/Plan";
 import "./Staking.css";
 
 const Staking = () => {
   const {
     state: { contract, accounts },
   } = useEth();
-  const [amount, setAmount] = useState("0");
+  const [amount, setAmount] = useState(0);
+  const [stakeAmount, setStakeAmount] = useState(0);
 
-  const sendAllTokens = () => {};
+  const stakingPlans = [
+    {
+      title: "Plan 1",
+      lockPeriod: 6,
+      apr: 10,
+      minAmount: 1000,
+      maxAmount: 1000000,
+      tokenDeposit: 10000,
+      stakerTotal: 654,
+    },
+    {
+      title: "Plan 2",
+      lockPeriod: 12,
+      apr: 17,
+      minAmount: 500,
+      maxAmount: 2500000,
+      tokenDeposit: 1090000,
+      stakerTotal: 3561,
+    },
+    {
+      title: "Plan 3",
+      lockPeriod: 24,
+      apr: 25,
+      minAmount: 500,
+      maxAmount: 5000000,
+      tokenDeposit: 2678900,
+      stakerTotal: 11309,
+    },
+  ];
+
+  const handleStakeInputChange = (e) => {
+    if (/^\d+$|^$/.test(e.target.value)) {
+      setStakeAmount(e.target.value);
+    }
+  };
+
+  const sendAllTokens = () => {
+    console.log({ amount });
+    setStakeAmount(amount);
+  };
+
   const handleStaking = async () => {
     try {
-      await contract.methods.stake().call();
-      await contract.methods.stake();
+      await contract.methods.stake().call({
+        from: accounts[0],
+      });
+      await contract.methods.stake().send({
+        from: accounts[0],
+      });
     } catch (e) {
       console.error(e);
     }
   };
 
   useEffect(() => {
-    console.debug("JARVIS");
-    const initUserTokenAmount = () => {
-      setAmount(1500);
-      console.debug({ amount });
-    };
-
-    initUserTokenAmount();
+    console.log("JARVIS");
+    setAmount(1500);
+    console.log({ amount });
   }, [contract, accounts]);
 
   return (
     <div id="staking">
       <h1 className="text-center p-3">Programme de staking du MLE</h1>
 
-      <section className="container d-flex justify-content-evenly text-center pt-3">
-        <div className="bloc col-3 staking-1 p-3">
-          <h3>Plan 1</h3>
-          <p>
-            Periode de blocage : 6 mois
-            <br />
-            APR : 10%
-            <br />
-            Minimum deposable : 500 MLE <br />
-            Maximum de tokens deposables : 1 000 000 MLE
-          </p>
-          <hr />
-          <p>
-            Nombre de tokens deposes : 10 000 MLE
-            <br />
-            Nombre de staker : 654
-          </p>
-        </div>
-        <div className="bloc col-3 staking-2 p-3">
-          <h3>Plan 2</h3>
-          <p>
-            Periode de blocage : 12 mois
-            <br />
-            APR : 16%
-            <br />
-            Minimum deposable : 1500 MLE
-            <br />
-            Maximum de tokens deposables : 2 500 000 MLE
-          </p>
-          <hr />
-          <p>
-            Nombre de tokens deposes : 3500 MLE
-            <br />
-            Nombre de staker : 4612
-          </p>
-        </div>
-        <div className="bloc col-3 staking-1 p-3">
-          <h3>Plan 3</h3>
-          <p>
-            Periode de blocage : 24 mois
-            <br />
-            APR : 20%
-            <br />
-            Minimum deposable : 1000 MLE
-            <br />
-            Maximum de tokens deposables : 5 000 000 MLE
-          </p>
-          <hr />
-          <p>
-            Nombre de tokens deposes : 1 890 000 MLE
-            <br />
-            Nombre de staker : 11309
-          </p>
-        </div>
+      <section className="container d-flex justify-content-evenly text-center py-5">
+        <Plan
+          title={stakingPlans[0].title}
+          lockPeriod={stakingPlans[0].lockPeriod}
+          apr={stakingPlans[0].apr}
+          minAmount={stakingPlans[0].minAmount}
+          maxAmount={stakingPlans[0].maxAmount}
+          tokenDeposit={stakingPlans[0].tokenDeposit}
+          stakerTotal={stakingPlans[0].stakerTotal}
+        />
+
+        <Plan
+          title={stakingPlans[1].title}
+          lockPeriod={stakingPlans[1].lockPeriod}
+          apr={stakingPlans[1].apr}
+          minAmount={stakingPlans[1].minAmount}
+          maxAmount={stakingPlans[1].maxAmount}
+          tokenDeposit={stakingPlans[1].tokenDeposit}
+          stakerTotal={stakingPlans[1].stakerTotal}
+        />
+
+        <Plan
+          title={stakingPlans[2].title}
+          lockPeriod={stakingPlans[2].lockPeriod}
+          apr={stakingPlans[2].apr}
+          minAmount={stakingPlans[2].minAmount}
+          maxAmount={stakingPlans[2].maxAmount}
+          tokenDeposit={stakingPlans[2].tokenDeposit}
+          stakerTotal={stakingPlans[2].stakerTotal}
+        />
       </section>
       <hr />
-      <section className="container d-flex justify-content-evenly text-center pt-3">
+      <section className="container d-flex justify-content-evenly text-center py-5">
         <div className="bloc col-3 staking-1 p-3">
           <h3>Plan 1</h3>
           <div className="mb-3">
             <label htmlFor="userTokenAmount" className="form-label">
               Token :{" "}
-              <a href="#" onClick={sendAllTokens}>
-                1500
-              </a>
+              <span className="walletAmount" onClick={sendAllTokens}>
+                {amount}
+              </span>
             </label>
           </div>
           <label htmlFor="userTokenStakeAmount" className="form-label">
@@ -109,7 +127,8 @@ const Staking = () => {
               className="form-control"
               id="exampleFormControlInput1"
               placeholder="MLE"
-              defaultValue={amount}
+              value={stakeAmount}
+              onChange={handleStakeInputChange}
             />
             <span className="input-group-text">MLE</span>
           </div>

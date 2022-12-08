97523358f59073ad78de39426ac7dc60f829792b
cyril marois <cyrilmarois@gmail.com>
2022-12-08 16:30:12 +0100

    feat(staking): Work on staking

--- client/src/components/UI/Main/Main.jsx
+++ client/src/components/UI/Main/Main.jsx
@@ -5,15 +5,17 @@ import NoticeNoArtifact from "../Notice/NoticeNoArtifact";
 import Home from "../../Pages/Home/Home";
 import Login from "../../Pages/Login/Login";
 import Staking from "../../Pages/Staking/Staking";
+import Formation from "../../Pages/Formation/Formation";
 
 const Main = () => {
   return (
     <main>
-      {/* <NoticeWrongNetwork />
-      <NoticeNoArtifact /> */}
-      <Home />
+      {/* <NoticeWrongNetwork /> */}
+      {/* <NoticeNoArtifact /> */}
+      {/* <Home /> */}
+      {/* <Formation /> */}
       {/* <Login /> */}
-      {/* <Staking /> */}
+      <Staking />
     </main>
   );
 };

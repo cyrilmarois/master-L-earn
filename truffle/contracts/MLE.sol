4c09893a02e3f5a8ad655c408226282509ae897d
Cyril Marois <cyrilmarois@gmail.com>
2022-12-10 02:20:03 +0100

    fix(staking): fix withdraw funds condition

--- truffle/contracts/MLE.sol
+++ truffle/contracts/MLE.sol
@@ -72,7 +72,7 @@ contract MLE is ERC20, ERC20Votes, Ownable {
         Announce[] announces;
     }
 
-    struct stakingDepositRecord {
+    struct stakingRecord {
         uint256 date;
         uint256 amount;
     }
@@ -81,7 +81,7 @@ contract MLE is ERC20, ERC20Votes, Ownable {
         uint8 planId;
         uint8 apr;
         uint128 lockPeriod;     // exprimed in seconds
-        uint128 totalStakers;
+        uint256 totalStakers;
         uint256 totalStakingDeposit;
         uint256 minTokenAmount; // exprimed in MLE
         uint256 maxTokenDeposit; // exprimed in MLE
@@ -498,44 +498,6 @@ contract MLE is ERC20, ERC20Votes, Ownable {
         emit StakeDeposit(_amount, msg.sender, _planId, newUserStakingDepositBalanceTotal);
     }
 
-    function _afterDepositStakingTransfer(uint _planId, uint _amount) internal returns (uint) {
-        // register deposit
-        stakingRecord memory userStakingDeposit = stakingRecord({
-            date: block.timestamp,
-            amount: _amount
-        });
-        userStakingDepositBalance[msg.sender][_planId].push(userStakingDeposit);
-        stakingRecord[] memory deposits = userStakingDepositBalance[msg.sender][_planId];
-
-        // update staking plan stakers total
-        if (deposits.length == 1) {
-            stakingPlans[_planId].totalStakers += 1;
-        }
-
-        // update total deposit from all users
-        stakingPlans[_planId].totalStakingDeposit += _amount;
-
-        // update stakers
-        registerStakers();
-
-        // update user total staking deposit balance
-        uint256 newUserStakingDepositBalanceTotal;
-        for (uint i = 0; i < deposits.length; i++) {
-            newUserStakingDepositBalanceTotal += deposits[i].amount;
-        }
-
-        return newUserStakingDepositBalanceTotal;
-    }
-
-    function registerStakers() internal {
-        for (uint i =0; i < stakers.length; i++) {
-            if (stakers[i] == msg.sender) {
-                return;
-            }
-        }
-        stakers.push(msg.sender);
-    }
-
     function stakeWithdraw(uint256 _amount, uint8 _planId) external {
         require(_planId <= 1, "Error, staking plan does not exists");
 
@@ -548,6 +510,42 @@ contract MLE is ERC20, ERC20Votes, Ownable {
         emit StakeWithdrawal(_amount, msg.sender, _planId, newUserStakingBalanceTotal);
     }
 
+    function _afterDepositStakingTransfer(uint _planId, uint _amount) internal returns (uint) {
+        // register deposit
+        stakingRecord memory userStakingDeposit = stakingRecord({
+            date: block.timestamp,
+            amount: _amount
+        });
+        userStakingDepositBalance[msg.sender][_planId].push(userStakingDeposit);
+
+        // update total deposit from all users
+        stakingPlans[_planId].totalStakingDeposit += _amount;
+
+        // update stakers
+        _registerStakers();
+
+        // update staking plan stakers total
+        stakingPlans[_planId].totalStakers = stakers.length;
+
+        // update user total staking deposit balance
+        uint256 newUserStakingDepositBalanceTotal;
+        stakingRecord[] memory deposits = userStakingDepositBalance[msg.sender][_planId];
+        for (uint i = 0; i < deposits.length; i++) {
+            newUserStakingDepositBalanceTotal += deposits[i].amount;
+        }
+
+        return newUserStakingDepositBalanceTotal;
+    }
+
+    function _registerStakers() internal {
+        for (uint i =0; i < stakers.length; i++) {
+            if (stakers[i] == msg.sender) {
+                return;
+            }
+        }
+        stakers.push(msg.sender);
+    }
+
     function _beforeWithdrawalStakingTransfer(uint _planId, uint _amount) internal view returns (uint) {
         // count withdraw amount
         uint256 userStakingWithdrawalBalanceTotal = _getUserWithdrawalBalance(_planId);
@@ -558,7 +556,7 @@ contract MLE is ERC20, ERC20Votes, Ownable {
         (userStakingDepositBalanceTotal, firstDepositDate) = _getUserStakingDepositBalance(_planId);
 
         uint256 userStakingBalanceTotal = userStakingDepositBalanceTotal - userStakingWithdrawalBalanceTotal;
-        require(userStakingBalanceTotal  - _amount <= 0, "Error, insuffisant funds in staking to withdraw");
+        require(userStakingBalanceTotal  - _amount >= 0, "Error, insuffisant funds in staking to withdraw");
 
         // uint256 lockPeriod = stakingPlans[_planId].lockPeriod;
         // require(block.timestamp > firstDepositDate + lockPeriod, "Error, You can't withdraw before lockPeriod");
@@ -613,7 +611,7 @@ contract MLE is ERC20, ERC20Votes, Ownable {
     }
 
     function deleteStakers() internal {
-        for (uint i =0; i < stakers.length; i++) {
+        for (uint i = 0; i < stakers.length; i++) {
             if (stakers[i] == msg.sender) {
                 delete(stakers[i]);
             }

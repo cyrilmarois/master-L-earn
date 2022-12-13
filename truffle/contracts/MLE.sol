40dde8bda2007b6f40b9608a51a489fc55011109
Cyril Marois <cyrilmarois@gmail.com>
2022-12-13 12:16:03 +0100

    feat(contract): Update error messages

--- truffle/contracts/MLE.sol
+++ truffle/contracts/MLE.sol
@@ -50,11 +50,12 @@ contract MLE is ERC20, ERC20Votes, Ownable {
     constructor() ERC20("Master L&Earn", "MLE") Ownable() ERC20Permit("Master L&Earn") {
         _mint(msg.sender, INITIAL_SUPPLY);
         mleStaking = new MLEStaking();
-        //0xD98c5d2949653CDb3F8935802E208E87C71dc7f7
-        address teacher1 = 0x97635a8AEcB3CfcaE96F7a11Ec6A0AB213E3F065;
-        address student1 = 0x5F33213C35933CEe1E32e8796710D24CfC844ddf;
-        address student2 = 0x51B58B2583c49De79bbaf762F7EeAf1cbC33b7Ce;
-        address recruiter1 = 0x4F76FD57f86C14fAa7d4DfF8EdD9986d982BA569;
+        //0x49B912A15B7d277812CC097a9DFB8c9732476580
+        address teacher1 = 0xEEf02CF3Dd4d8719aFd331271Edc797eE1Ff077e;
+        address teacher2 = 0x754dfb5C73D38d4aF7475B5244AA57EB5013556c;
+        address student1 = 0xbEA6C00E57CFCF1Ad5e20c6977498ca8dD7b6ca1;
+        address student2 = 0xc3d2860Ca5Ca7feB0C6345bA87b077CEfA7aA86C;
+        address recruiter1 = 0x4E5FC164295839C457B5C2fB167cbF092a73c346;
         // address teacher1 = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
         // address student1 = 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db;
         // address student2 = 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB;
@@ -62,7 +63,9 @@ contract MLE is ERC20, ERC20Votes, Ownable {
 
         //teacher
         transfer(teacher1, 1000e18);
+        transfer(teacher2, 1000e18);
         registerUser(teacher1, false, true, false);
+        registerUser(teacher2, false, true, false);
 
         //student
         transfer(student1, 1000e18);
@@ -329,7 +332,7 @@ contract MLE is ERC20, ERC20Votes, Ownable {
         string memory _title,
         string[] memory _tags
     ) external {
-        require (transfer(address(this), announcePostPrice), "Not enough tokens");
+        require (transfer(address(this), announcePostPrice), "Insufficient balance");
         address[] memory candidates;
         recruiters[msg.sender].announces.push( MLEUtils.Announce (
             true,
